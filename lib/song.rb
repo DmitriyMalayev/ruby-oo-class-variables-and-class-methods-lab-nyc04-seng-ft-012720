e1813c16fd5bad36eb27f5bc4ceadd62c26c2985
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-01 20:11:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -48,18 +48,4 @@ def self.genre_count
     end
     artists
   end
-
-
-
-
-def ninety_nine_problems
-@ninety_nine_problems
-end
-
-def ninety_nine_problems=(name, artist, genre)
-@name = name
-@artist = artist
-@genre = genre
-end
-
 end
