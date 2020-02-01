43f7b44556622ceaa5089f88b6a21cd2afc5fd90
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-02-01 19:02:09 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+def initialize(name, artist, genre)
+  @name = name  
+  @artist = artist 
+  @genre = genre 
+end 
+
+def ninety_nine_problems 
+@ninety_nine_problems 
+end 
+
+def ninety_nine_problems=(name, artist, genre)
+@name = name 
+@artist = artist 
+@genre = genre 
+end 
+
+
+
+def 
+
+
+
+
+
+
+  
