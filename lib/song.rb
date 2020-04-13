0b6ee2ceed926dcaf9e77dad7b419016dd02f8ab
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-04-13 01:21:16 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,51 +1,63 @@
-class Song
-  attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@artists = []
-  @@genres = []
+require "pry"
 
-def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
-  @@count += 1
-  @@artists << artist
-  @@genres << genre
-end
+class Song 
+    @@all = [] 
+    @@genres = []
+    @@artists = []
+    @@count = 0 
 
-def self.count
-@@count
-end
 
-def self.genres
-  @@genres.uniq
-end
+    attr_accessor :name, :artist, :genre 
 
-def self.artists
-  @@artists.uniq
-end
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist 
+        @@artists << @artist 
 
-def self.genre_count
-    genres = {}
-    @@genres.each do |g|
-      if genres[g]
-         genres[g] += 1
-       else
-         genres[g] = 1
-      end
-    end
-    genres
-  end
+        @genre = genre 
+        @@genres << @genre
+        
+        @@count +=1 
 
-  def self.artist_count
-    artists = {}
-    @@artists.each do |a|
-      if artists[a]
-        artists[a] += 1
-      else
-        artists[a] = 1
-      end
-    end
-    artists
-  end
-end
+        @@all << self 
+    end 
+
+    def self.count 
+        @@count
+    end 
+
+    def self.genres 
+        @@genres.uniq 
+    end 
+
+    def self.artists
+        @@artists.uniq 
+    end 
+
+    def self.genre_count
+     genres = {} 
+     @@genres.each do |gen|
+        if genres[gen]
+          genres[gen] += 1
+          #binding.pry 
+        else  
+          genres[gen] = 1   
+        end 
+      end 
+      genres 
+    end 
+
+    def self.artist_count
+      artists = {} 
+      @@artists.each do |art|
+        if artists[art] 
+          artists[art] += 1
+        else 
+          artists[art] = 1 
+        end    
+      #binding.pry 
+    end 
+    artists 
+  end 
+
+end 
